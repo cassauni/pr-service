d1e8031d18984ba72aa83abdf90a16ab9a98ac6d
cassauni <vasaby2004@gmail.com>
2025-11-18 16:06:24 +0300

    yaml delete

--- config/model.go
+++ config/model.go
@@ -1,21 +1,21 @@
 package config
 
 type ConfigModel struct {
-	HTTP     HTTPConfig     `yaml:"HTTP"`
-	Postgres PostgresConfig `yaml:"Postgres"`
+	HTTP     HTTPConfig
+	Postgres PostgresConfig
 }
 
 type PostgresConfig struct {
-	Host     string `yaml:"host"`
-	Port     string `yaml:"port"`
-	User     string `yaml:"user"`
-	Password string `yaml:"password"`
-	DBName   string `yaml:"DBName"`
-	SSLMode  string `yaml:"sslMode"`
-	PgDriver string `yaml:"pgDriver"`
+	Host     string
+	Port     string
+	User     string
+	Password string
+	DBName   string
+	SSLMode  string
+	PgDriver string
 }
 
 type HTTPConfig struct {
-	Host string `yaml:"host" validate:"required"`
-	Port string `yaml:"port" validate:"required"`
+	Host string
+	Port string
 }
